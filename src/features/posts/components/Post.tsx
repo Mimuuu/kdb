5e025a752e9a030041397fea0384dcaac5bce04e
Brice <brice@rekki.com>
2021-06-20 12:16:50 +0200

    add comments visibility toggling in post

--- src/features/posts/components/Post.tsx
+++ src/features/posts/components/Post.tsx
@@ -1,17 +1,60 @@
 import React from "react";
+import { get } from "../../../app/utils/http";
 import { Post as PostType } from "../types";
+import { Comment as CommentType } from "../types";
 import styles from "./Post.module.scss";
 
 interface Props {
   p: PostType;
 }
 
+function renderComments(comments: CommentType[] | undefined, error: string) {
+  if (error) return <p className={styles.error}>failed to load comments</p>;
+  if (!comments) return <p>...</p>;
+  return 
+}
+
 function Post({ p }: Props) {
+  const [comments, setComments] = React.useState<CommentType[] | undefined>(undefined);
+  const [error, setError] = React.useState<string>("");
+  const [showComments, setShowComments] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    async function loadComments() {
+      const r = await get(`https://jsonplaceholder.typicode.com/posts/${p.id}/comments`)
+      if (r.error) return setError("failed to load comments");
+      setComments(r);
+    }
+
+    loadComments();
+  }, []);
+
+  const toggleComments = () => setShowComments(!showComments);
+
+  const toggleAction = showComments ? "hide" : "show";
+
   return (
     <div className={styles.container}>
       <p className={styles.title}>{p.title}</p>
       <p className={styles.body}>{p.body}</p>
+
+      <div className={styles.commentsContainer}>
+        {error && (<p className={styles.error}>failed to load comments</p>)}
+        {!comments && !error && (<p className={styles.error}>...</p>)}
+        {comments && (
+          <p className={styles.toggle} onClick={toggleComments}>{toggleAction} {comments.length} comments</p>
+        )}
+
+
+        {comments && (
+        <div className={`${styles.comments} ${showComments && styles.visible}`}>
+          {/* {comments.map(c => <Comment key={c.id} c={c} />)} */}
+          {comments.map(c => <p key={c.id}>{c.id}</p>)}
+        </div>
+      )}
+      </div>
     </div>
   );
 }
+
 export default Post;
